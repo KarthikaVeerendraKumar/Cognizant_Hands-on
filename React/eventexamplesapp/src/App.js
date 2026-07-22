ecfdd1b1940b43628a9d4142f42cdf7f0c20b453
Karthika VeerendraKumar <karthikaveerendran@gmail.com>
2026-07-22 18:01:50 +0530

    ReactJS-HOL

---
+++ React/eventexamplesapp/src/App.js
@@ -0,0 +1,24 @@
+import EventExamples from './Components/EventExamples';
+import CurrencyConvertor from './Components/CurrencyConvertor';
+
+function App() {
+
+  return (
+
+    <div style={{ padding: "20px" }}>
+
+      <h1>React Event Examples</h1>
+
+      <EventExamples />
+
+      <hr />
+
+      <CurrencyConvertor />
+
+    </div>
+
+  );
+
+}
+
+export default App;
